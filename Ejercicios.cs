7e0cc3714287e7e5bbab6ef4095818593fe4c28b
Felipe Mora <lmora@growthaccelerationpartners.com>
2024-06-12 21:27:35 -0600

    ejercicio 1

---
+++ Ejercicios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClase
+{
+    /// <summary>
+    /// Ejercicios de clase.
+    /// </summary>
+    public static class Ejercicios
+    {
+        /// <summary>
+        /// Gets the menor element in a list.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>the minor element.</returns>
+        public static int GetMenor(List<int> input)
+        {
+            if (input == null || input.Count == 0)
+            {
+                throw new ArgumentException("The input is null or empty");
+            }
+
+            var result = input[0];
+            for (var index = 1; index < input.Count; index++)
+            {
+                if (input[index] < result)
+                {
+                    result = input[index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace DemoClase
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            var list = new List<int> { 10, 2, 3, 54, 5, 6, 9, 1 };
+            
+            var menor = Ejercicios.GetMenor(list);
+
+            Console.WriteLine($"El menor es: {menor}");
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
