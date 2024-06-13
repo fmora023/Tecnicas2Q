5e0615900f2369ceb08bc4bee4adc2f8f9e15a7e
Felipe Mora <lmora@growthaccelerationpartners.com>
2024-06-12 19:35:34 -0600

    initial commit - setup repo - add sln

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace DemoClase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tecnicas2Q
 Curso tecnicas de programacion 2Q
+
+## Semana 1
+Creacion de repo
+Ejercicios introdutorios a **C#**
